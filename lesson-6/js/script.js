935385ea12e7b31b9ccc431f231430a3034c8d71
Lyubomyr89 <lyubchukgorbatyuk@gmail.com>
2021-02-15 19:50:45 +0200

    Функції

---
+++ lesson-6/js/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const x = 25;
+const y = 30;
+
+
+
+const sumAdd = function (a = 5, b = 4, c = 2) {
+    // const sum = ;
+
+    // return x + y;
+    return (
+        a + b * c
+    );
+}
+
+console.log(sumAdd(15, 10, 4));
+console.log(sumAdd(12, 10, 4));
+console.log(sumAdd(10, 10, 4));
+
+// sumNumber();
+
+// function sumNumber(){
+//     const sum = x + y;
+//     console.log(sum);
+// }
