1e6308543fca9e3ec44efa144bfcd332ed6e96be
Lyubomyr89 <lyubchukgorbatyuk@gmail.com>
2021-03-01 21:17:10 +0200

    Об'єкти

---
+++ m3.lesson-1/js/script.js
@@ -0,0 +1,44 @@
+'use strict'
+
+// const car = ['Name', 'color', 'model', 'year'];
+let keys = 4;
+let doors = 5;
+
+const car = {
+    //key: value,
+    //key: value,
+    name: 'BMW',
+    year: 2018,
+    model: 'X6',
+    color: 'red',
+    owners: ['Max', 'Jon'],
+    age: function () {
+        return 2021 - this.year;  //this = car
+    },
+    'user-name': 'Admin',
+    keys,
+    doors,
+    price: 5000,
+}
+
+    car.year = 2016; //Зміна значення властивості об'єкта
+
+console.log(`Ваш автомобіль - ${car.name}, модель - ${car.model}, рік випуску - ${car.year}, вік автомобіля - ${car.age()} років, 
+            попередні власники ${car.owners}`);
+
+            delete car.owners; //Видалення значення властивості об'єкта
+
+            car.speed = 250; //Створення нової властивості об'єкта
+            console.log(car.speed);
+                
+
+            console.log(car["user-name"]);
+            
+            console.log(car.keys);
+            console.log(car.doors);
+            console.log(car);
+            
+            
+            // for(), for...of, for..in
+            
+            
