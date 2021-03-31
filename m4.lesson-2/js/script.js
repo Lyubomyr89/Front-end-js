502694ab59f245ddff948dc312cab46cc80df0ee
Lyubomyr89 <lyubchukgorbatyuk@gmail.com>
2021-03-31 19:47:06 +0300

    замикання,лексичне оточення

--- m4.lesson-2/js/script.js
+++ m4.lesson-2/js/script.js
@@ -47,13 +47,68 @@ let counter_1 = document.querySelector('.count-btn-1'),
     counter_2 = document.querySelector('.count-btn-2'),
     counter_3 = document.querySelector('.count-btn-3');
 
-let a = 0;
 
-function counter() {
-    a++;
-    console.log(a);
+
+function counter(selector) {
+    let i = 0;
+    const res = document.querySelector(selector);
+
+    return function (){
+        i++;
+
+        if(res){
+            res.innerText = i;
+        }
+        
+        console.log(i);        
+    }
 }
 
-counter_1.onclick = counter;
-counter_2.onclick = counter;
-counter_3.onclick = counter;
+// console.log(counter());
+
+
+let d = counter(); //
+let a = counter();
+
+d();
+// d();
+// d();
+console.log('-----------');
+
+a();
+a();
+a();
+
+console.log('-----------');
+// let b = counter();
+let x = 0; //let i = 0;
+let b = function (){
+    x++;
+    console.log(x);
+};
+
+
+counter_1.onclick = counter('.count-result-1');
+counter_2.onclick = counter('.count-result-2');
+counter_3.onclick = b;
+
+
+
+// let i = 0;
+
+// counter_1.onclick = function (){
+//         i++;
+//         console.log(i);        
+//     };
+
+//     let i2 = 0;
+// counter_2.onclick = function (){
+//         i2++;
+//         console.log(i2);        
+//     };
+
+//     let i3 = 0;
+// counter_3.onclick = function (){
+//         i3++;
+//         console.log(i3);        
+//     };
